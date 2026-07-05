0ec467dbf3eae51d53ac88a4165a8350a5f7e817
Aman Bhati <bhatiaman123.ab@gmail.com>
2026-07-05 08:48:44 +0530

    Fix Netlify paths

--- animated-bouquet-main/next.config.mjs
+++ animated-bouquet-main/next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
-  basePath: "/animated-bouquet",
-  assetPrefix: "/animated-bouquet/",
   images: {
     unoptimized: true,
   },
